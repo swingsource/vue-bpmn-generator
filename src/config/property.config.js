61822340988acb29ad1c56dad614743ceec17d3d
郝鹏 <swingsource@163.com>
2021-08-11 19:04:40 +0800

    condition property 配置项

---
+++ src/config/property.config.js
@@ -0,0 +1,29 @@
+export const conditionList = [
+  {
+    key: 'remiAmount',
+    label: '报销金额'
+  },
+  {
+    key: 'budgetAmount',
+    label: '预算金额'
+  }
+]
+
+export const operatorList = [
+  {
+    key: '>',
+    label: '大于'
+  },
+  {
+    key: '>=',
+    label: '大于等于'
+  },
+  {
+    key: '<',
+    label: '小于'
+  },
+  {
+    key: '<=',
+    label: '小于等于'
+  }
+]
